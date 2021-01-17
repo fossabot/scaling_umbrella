7fd4f25e143763901b39bfc478b3b7e97dfa34bb
Joe Becher <me@drazisil.com>
2021-01-16 20:49:24 -0500

    Add methods to load and validate xml and dtd

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import argparse
+from io import FileIO
+
+from lxml import etree
 parser = argparse.ArgumentParser()
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
     return name
 
 
-# Press the green button in the gutter to run the script.
+def load_dtd():
+    dtd = etree.DTD(FileIO('schema/coverage-04.dtd'))
+    print(dtd)
+    return dtd
+
+
+def xml_from_file(file_path):
+    tree = etree.parse(file_path)
+    print(file_path, etree.tostring(tree.getroot()))
+    return tree
+
+
+def validate_xml(dtd, xml):
+    print(dtd.validate(xml))
+
+
 if __name__ == '__main__':
     parser.add_argument('file1', help='first file to check')
-    parser.parse_args()
+    args = parser.parse_args()
     print_hi('PyCharm')
+    dtd = load_dtd()
+    xml = xml_from_file(args.file1)
+    validate_xml(dtd, xml)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
